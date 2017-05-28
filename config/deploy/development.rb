75e0bd669c786a7532cf921469de7f1d0221ee32
tonnamb <tonnam.balankura@gmail.com>
2017-05-28 14:39:43 -0400

    first try to deployment

---
+++ config/deploy/development.rb
@@ -0,0 +1,16 @@
+set :development, :develop
+set :unicorn_rack_env, "development"
+set :rails_env, 'development'
+set :migration_role, 'db'
+
+role :app, %w{ec2-34-211-25-138.us-west-2.compute.amazonaws.com}
+role :web, %w{ec2-34-211-25-138.us-west-2.compute.amazonaws.com}
+role :db, %w{ec2-34-211-25-138.us-west-2.compute.amazonaws.com}
+
+set :ssh_options, {
+  keys: [File.expand_path('~/.ssh/capistrano.pem')],
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
+
+server 'ec2-34-211-25-138.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}
